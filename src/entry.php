c43d6647008ae78206eac5587b2202be887de298
fujirai <2201188@s.asojuku.ac.jp>
2024-01-15 16:07:55 +0900

    h

--- src/entry.php
+++ src/entry.php
@@ -6,10 +6,10 @@
         <title>データ追加</title>
     </head>
     <body>
-        <p>商品を追加します。</p>
+        <h1>商品を追加します。</h1>
         <form action="entry_output.php" method="post">
-            お茶名<input type="text" name="name"></br>
-            価格<input type="text" name="price"></br>
+            <p>お茶名<input type="text" name="name"></p>
+            <p>価格<input type="text" name="price"></p>
             <button type="submit">追加</button>
         </form>
     </body>
